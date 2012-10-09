959dff6cafd4741237468f2f28f3a4718067f445
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2012-10-09 10:59:32 +0100

    Initial commit

---
+++ TODO.markdown
@@ -0,0 +1,7 @@
+To Do 
+=====
+
+Change styling to be DRY
+Add users model and associate books to owners
+Add signin/signout
+Create a requests system
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booksiveread::Application.config.secret_token = '32cc4d9bf4bf4f4f09f5c90e2ca299008997ef6200d18cfce2cfd174d0bb8dcf6da1855e190192d5cf040a41a3681b22dc460a4f661954c0b4d4d9ef0c7b0d1c'
